11fad370f0ada0c1c50c9308aa0a5709f1777c1d
Laura <pizzuttilaura@gmail.com>
2024-07-12 10:17:47 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import './components/Login';
+import './components/Gerenciamento';
+import './components/Forms';
+import './components/Temas';
+import './components/To-do';
+import './components/Time';
+import './components/Fatorial';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
         
       </header>
+      <main className="main">
+
+
+
+      </main>
+      <footer className="footer">
+
+      </footer>
     </div>
   );
 }
